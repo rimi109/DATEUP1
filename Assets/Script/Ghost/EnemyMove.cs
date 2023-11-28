275909973413f99f53763130fe205240edff7368
rimi109 <108114019+rimi109@users.noreply.github.com>
2023-11-28 14:14:15 +0900

    [Update]
    お化けのスクリプトを少し手直し

--- Assets/Script/Ghost/EnemyMove.cs
+++ Assets/Script/Ghost/EnemyMove.cs
@@ -4,17 +4,12 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMove : MonoBehaviour
 {
+    [SerializeField]
     private NavMeshAgent navMeshAgent;
 
-    private void Start()
-    {
-        navMeshAgent = GetComponent<NavMeshAgent>(); // NavMeshAgentを保持しておく
-    }
-
-    // CollisionDetector.csのonTriggerStayにセットし、衝突中に実行される。
     public void OnDetectObject(Collider collider)
     {
-        // 検知したオブジェクトに「Player」のタグがついていれば、そのオブジェクトを追いかける
+     
         if (collider.CompareTag("Player"))
         {
             navMeshAgent.destination = collider.transform.position;
